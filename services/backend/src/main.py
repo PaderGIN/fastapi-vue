9edad7a209b5a3b30b1157aefe870e0debd0c03d
Gleb Padergin <glebpad@yandex.ru>
2022-08-04 20:31:19 +0500

    Initial commit

---
+++ services/backend/src/main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from tortoise import Tortoise
+
+from database.register import register_tortoise
+from database.config import TORTOISE_ORM
+
+# enable schemas to read relationship between models
+Tortoise.init_models(["services.backend.src.database.models.py"], "models")
+
+"""
+import 'from src.routes import users, notes' must be after 'Tortoise.init_models'
+why?
+https://stackoverflow.com/questions/65531387/tortoise-orm-for-python-no-returns-relations-of-entities-pyndantic-fastapi
+"""
+from services.backend.src.routes import users, notes
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8080"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+app.include_router(users.router)
+app.include_router(notes.router)
+
+register_tortoise(app, config=TORTOISE_ORM, generate_schemas=False)
+
+
+@app.get("/")
+def home():
+    return "Hello, World!"
